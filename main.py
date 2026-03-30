128e65be2d2b27b585b1957e06687fdc3a204318
clairekiu <clairekuw@gmail.com>
2026-03-31 02:01:06 +0900

    initial

---
+++ main.py
@@ -0,0 +1,11 @@
+def main():
+    print("Let's implement division.")
+    x = int(input("x > "))
+    y = int(input("y > "))
+    print(f"{x} / {y} = {divide(x, y)}")
+
+def divide(x, y):
+    if y == 0:
+        print("Error: cannot divide by zero.")
+    else:
+        return x/y
